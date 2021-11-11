e16bf6ac4408bfc175de76d5bb5ed5dd7d59421b
Stud. Gabriele Garigliano <g.garigliano.1312@vallauri.net>
2021-11-11 09:02:57 +0100

    test remote 2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function serverRequestHandler(req, res)
 {
     res.writeHead(200, {ContentType: 'text/html'});
 
-    //checks if the request was a client page requests
+    //checks if the request was a client page request
     if(client.isInURLTree(req.url))
         res.write(await client.loadPage(req.url));
 
