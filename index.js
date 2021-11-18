2d901869e4e9de61e6c87b8c82b38e693f7943db
Ghen <gabriele.garigliano@gmail.com>
2021-11-18 10:42:28 +0100

    mega bocciato professor mana hahah

--- index.js
+++ index.js
@@ -1,19 +1,63 @@
-const http = require('http');
-const { readFileSync } = require('fs').promises;
-const client = require('./server/client');
+var questions;
+var askedQuestionsIds;
+var currQuestion;
 
-// server setup
-
-const server = http.createServer(serverRequestHandler);
-
-async function serverRequestHandler(req, res)
+window.onload = function()
 {
-    //checks if the request was a client page request
-    if(client.isInURLTree(req.url))
-        res.write(await client.loadPage(req.url, res));
+    questions = JSON.parse(window.localStorage.getItem("questions"));
+    if(questions == undefined)
+        questions = [];
+    askedQuestionsIds = [];
 
-    //sends the response (HAS TO BE THE LAST ROW)
-    res.end();
+    LoadAnswer();
 }
 
-server.listen(80);
+function InsertNew()
+{
+    questions = JSON.parse(window.localStorage.getItem("questions"));
+    if(questions == undefined)
+        questions = [];
+
+    questions.push({question: $("#getQuestion").val(), answer: $("#getAnswer").val().split(',')});
+    console.log(questions);
+    window.localStorage.setItem("questions", JSON.stringify(questions));
+}
+
+function Answer()
+{
+
+    if(currQuestion.answer.includes($("#answer").val()))
+    {
+        console.log(`Correct! Answer ${$("#answer").val()}`);
+        console.log(currQuestion.answer);
+        LoadAnswer();
+    }
+}
+
+function LoadAnswer()
+{
+    if(askedQuestionsIds.length != questions.length)
+    {
+        let questionId;
+        do
+            questionId = Math.floor(Math.random() * questions.length);
+        while(askedQuestionsIds.includes(questionId));
+    
+        currQuestion = questions[questionId];
+    
+        $("#question").text(currQuestion.question);
+    
+        askedQuestionsIds.push(questionId);
+    }
+}
+
+function Delete()
+{
+    questions = JSON.parse(window.localStorage.getItem("questions"));
+    if(questions == undefined)
+        questions = [];
+
+    questions = questions.filter((v, i, a) => { return v.question != $("#questionToDelete").val()});
+    console.log(questions);
+    window.localStorage.setItem("questions", JSON.stringify(questions));
+}
