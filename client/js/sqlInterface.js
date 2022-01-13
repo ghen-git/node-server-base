6d904b0a629d61d481794f4141f28fc6e169de81
ghen-git <gabriele.garigliano@gmail.com>
2022-01-13 09:36:33 +0100

    client improvement

---
+++ client/js/sqlInterface.js
@@ -0,0 +1,21 @@
+async function executeQuery(queryName)
+{
+    return new Promise(resolve =>
+        {
+            let ajaxRequest = $.ajax({url: `query/${queryName}`, type: "POST", timeout: "4000"});
+        
+            ajaxRequest.fail(() => this.console.error("Holy shmokes."));
+            ajaxRequest.done((data) =>{ resolve(data); });
+        });
+}
+
+async function executeParamQuery(queryName, param)
+{
+    return new Promise(resolve =>
+        {
+            let ajaxRequest = $.ajax({url: `query/${queryName}`, type: "POST", timeout: "4000", data: param});
+        
+            ajaxRequest.fail(() => this.console.error("Holy shmokes."));
+            ajaxRequest.done((data) =>{ resolve(data); });
+        });
+}
