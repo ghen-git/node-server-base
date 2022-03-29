f1720c745bd0d98ce815bfdd7cf8706f10366f4f
ghen-git <gabriele.garigliano@gmail.com>
2022-03-29 19:24:32 +0200

    json table optimization & tailwind CDN update

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,23 @@
 import sqlInterface from './modules/queryCalls.js';
+import JSONTable from './modules/JSONTable';
 
+//sql interface example
 sqlInterface.executeQuery('example');
+
+//JSON table example
+const json = 
+[
+    {_id: 1, name: 'Emily', surname: 'Parker', age: '25', last_met: '20yrs'},
+    {_id: 2, name: 'Toby', surname: 'McGuyre', age: '13', last_met: '1hr'},
+    {_id: 3, name: 'Thomas', surname: 'Elliot', age: '15', last_met: '3ds'},
+    {_id: 4, name: 'James', surname: 'Paul', age: '26', last_met: '5hrs'},
+    {_id: 5, name: 'Elliot', surname: 'Rafey', age: '70', last_met: '3yrs'}
+]
+const table = JSONTable.create(json, 
+    {
+        columnNames: ['Name', 'Surname', 'Age', 'Last Met'],
+        ignoreColumns: ['_id'],
+        mode: 'dark',
+        headerClasses: 'bg-orange-300 border-b-2 border-b-orange-400'
+    });
+$('body').append(table);
--- client/js/modules/JSONTable.js
+++ client/js/modules/JSONTable.js
@@ -3,11 +3,55 @@ Creates tables from JSON files using tailwindcss
 for graphics and jquery for the interaction with the DOM. Made because
 we are always asked to do the same fucking thing over and over im tired
 stop it stop it the voices in my head they're telling me to end it for good.
+
+options
+-
+columnNames ([]), ignoreColumns ([]), mode ("light"), tableClasses (""), 
+tbodyClasses (""), rowCallback (""), rowClasses (""), cellClasses (""), 
+headerClasses (""), headerCellsClasses ("")
 */
 
-//light mode
-function create(json, columnNames = [], ignoreColumns = [])
+function create(json, options)
 {
+    //options setup
+    const columnNames = options.columnNames != undefined ? options.columnNames : [];
+
+    const ignoreColumns = options.ignoreColumns != undefined ? options.ignoreColumns : [];
+
+    const mode = options.mode != undefined ? options.mode : 'light';
+
+    const tableClasses = options.tableClasses != undefined ? 
+    options.tableClasses :
+        mode == 'light' ? 'm-6 bg-white shadow-xl' : 
+        mode == 'dark' ? 'm-6 bg-white shadow-xl' : 
+    '';
+
+    const tbodyClasses = options.tbodyClasses != undefined ? 
+    options.tbodyClasses :
+        mode == 'light' ? '' : 
+        mode == 'dark' ? 'bg-gray-800' : 
+    '';
+
+    const rowClasses = options.rowClasses != undefined ? 
+    options.rowClasses : '';
+
+    const cellClasses = options.cellClasses != undefined ? 
+    options.cellClasses :
+        mode == 'light' ? 'p-3 px-5' : 
+        mode == 'dark' ? 'p-3 px-5 text-white' : 
+    '';
+    
+    const headerClasses = options.headerClasses != undefined ? 
+    options.headerClasses :
+        mode == 'light' ? 'bg-blue-100 border-b-2 border-b-blue-200' : 
+        mode == 'dark' ? 'bg-red-300 border-b-2 border-b-red-400' : 
+    '';
+    
+    const headerCellsClasses = options.headerCellsClasses != undefined ? 
+    options.headerCellsClasses : 'p-3 px-5';
+
+    const rowCallback = options.rowCallback;
+    
     if(json.length == 0)
         throw 'Invalid JSON, it must have at least one line';
 
@@ -15,32 +59,33 @@ function create(json, columnNames = [], ignoreColumns = [])
     //ignoreColumns
     const keys = ignoreColumns.length > 0 ? Object.keys(json[0]).filter(col => !ignoreColumns.includes(col)) : Object.keys(json[0]);
 
-    const table = $('<table class="m-6 bg-white shadow-xl">');
+    const table = $(`<table class="${tableClasses}">`);
 
-    const thead = $('<thead class="bg-blue-100 border-b-2 border-b-blue-200">');
+    const thead = $(`<thead class="${headerClasses}">`);
     
     if(columnNames.length > 0)
         for(const column of columnNames)
             thead.append($(`
-            <th class="p-3 px-5">${column}</th>
+            <th class="${headerCellsClasses}">${column}</th>
             `));
     else
         for(const column of keys)
             thead.append($(`
-            <th>${column.replace(/\w\S*/g, (txt) => {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();})}</th>
+            <th class="${headerCellsClasses}">${column.replace(/\w\S*/g, (txt) => {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();})}</th>
             `));
 
     table.append(thead);
 
-    const tbody = $('<tbody>');
+    const tbody = $(`<tbody class="${tbodyClasses}">`);
 
-    for(const row of json)
+    for(let i = 0; i < json.length; i++)
     {
-        const tr = $('<tr>');
+        const row = json[i];
+        const tr = $(`<tr class="${rowClasses}" onclick="${rowCallback != undefined ? rowCallback(i, row) : ''}">`);
         
         for(const col of keys)
             $(`
-            <td class="p-3 px-5">${row[col]}</td>
+            <td class="${cellClasses}">${row[col]}</td>
             `).appendTo(tr);
 
         tbody.append(tr);
@@ -51,50 +96,4 @@ function create(json, columnNames = [], ignoreColumns = [])
     return table;
 }
 
-//dark mode
-function createDark(json, columnNames = [], ignoreColumns = [])
-{
-    if(json.length == 0)
-        throw 'Invalid JSON, it must have at least one line';
-
-    //gets the cols of the json, filtering out any included in
-    //ignoreColumns
-    const keys = ignoreColumns.length > 0 ? Object.keys(json[0]).filter(col => !ignoreColumns.includes(col)) : Object.keys(json[0]);
-
-    const table = $('<table class="m-6 bg-white shadow-xl">');
-
-    const thead = $('<thead class="bg-red-300 border-b-2 border-b-red-400">');
-    
-    if(columnNames.length > 0)
-        for(const column of columnNames)
-            thead.append($(`
-            <th class="p-3 px-5">${column}</th>
-            `));
-    else
-        for(const column of keys)
-            thead.append($(`
-            <th>${column.replace(/\w\S*/g, (txt) => {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();})}}</th>
-            `));
-
-    table.append(thead);
-
-    const tbody = $('<tbody class="bg-gray-800">');
-
-    for(const row of json)
-    {
-        const tr = $('<tr>');
-        
-        for(const col of keys)
-            $(`
-            <td class="p-3 px-5 text-white">${row[col]}</td>
-            `).appendTo(tr);
-
-        tbody.append(tr);
-    }
-
-    table.append(tbody);
-
-    return table;
-}
-
-export default { create, createDark };
+export default { create };
